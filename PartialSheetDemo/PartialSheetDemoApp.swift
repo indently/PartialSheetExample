c1040fc20975d090e2aa69cfe63514c79cf33323
Code Palace <codepalace@fastmail.se>
2022-02-10 08:50:33 +0100

    Initial Commit

---
+++ PartialSheetDemo/PartialSheetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartialSheetDemoApp.swift
+//  PartialSheetDemo
+//
+//  Created by Federico on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PartialSheetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
